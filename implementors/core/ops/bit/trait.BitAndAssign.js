a733f5d4ecf6a9796eee9edcb611eba32627611e
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-04-15 05:08:43 +0200

    initial commit

---
+++ implementors/core/ops/bit/trait.BitAndAssign.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["gmime"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"gmime/struct.DecryptFlags.html\" title=\"struct gmime::DecryptFlags\">DecryptFlags</a>",synthetic:false,types:["gmime::auto::flags::DecryptFlags"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"gmime/struct.FilterBestFlags.html\" title=\"struct gmime::FilterBestFlags\">FilterBestFlags</a>",synthetic:false,types:["gmime::auto::flags::FilterBestFlags"]},];
+implementors["gmime_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"gmime_sys/struct.GMimeDecryptFlags.html\" title=\"struct gmime_sys::GMimeDecryptFlags\">GMimeDecryptFlags</a>",synthetic:false,types:["gmime_sys::GMimeDecryptFlags"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"gmime_sys/struct.GMimeFilterBestFlags.html\" title=\"struct gmime_sys::GMimeFilterBestFlags\">GMimeFilterBestFlags</a>",synthetic:false,types:["gmime_sys::GMimeFilterBestFlags"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"gmime_sys/struct.GMimeOpenPGPState.html\" title=\"struct gmime_sys::GMimeOpenPGPState\">GMimeOpenPGPState</a>",synthetic:false,types:["gmime_sys::GMimeOpenPGPState"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
